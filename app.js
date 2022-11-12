a4981dcc98740cf4721ae193b7a17f31dcb39a81
j10a1n15 <jan.leger@web.de>
2022-11-12 19:24:27 +0100

    Setup static files

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express")
 const app = express()
+const path = require("path")
 
 const { port } = require("./src/database/config.json")
 
 app.get("/", async function(req, res) {
-    res.send("HelloWorld")
+    res.sendFile(path.join(__dirname + "/src/web/index.html"))
 })
+
+app.use(express.static(path.join(__dirname, "src")))
 
 app.listen(port || 3000, () => {
     console.log(`App listening on Port ${port || 3000}!`)
---
+++ src/database/blocks.json.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "motion_movesteps": {
+        input: [
+            "number"
+        ],
+        category: "motion"
+    },
+        "motion_turnright": {
+            input: [
+                "number"
+            ],
+            category: "motion"
+        },
+        "motion_turnleft": {
+            input: [
+                "number"
+            ],
+            category: "motion"
+        },
+
+        "event_whenflagclicked": {
+            input: [],
+            category: "event"
+        }
+}
